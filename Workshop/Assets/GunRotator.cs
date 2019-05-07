d4fd382e592f819f33464d5c192bb88acddcbeb7
marcelloroehrssen <marcello.roehrssen@gmail.com>
2019-05-08 00:14:33 +0200

    step-4

---
+++ Workshop/Assets/GunRotator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunRotator : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0);
+        Vector3 lookPos = Camera.main.ScreenToWorldPoint(mousePos);
+
+        lookPos = lookPos - transform.position;
+        float angle = Mathf.Atan2(lookPos.y, lookPos.x) * Mathf.Rad2Deg;
+
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+}
