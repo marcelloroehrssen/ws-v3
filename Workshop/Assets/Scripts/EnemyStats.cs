e04798ea09997a29d1aa0e80838e9648c24f8af1
marcelloroehrssen <marcello.roehrssen@gmail.com>
2019-05-08 15:56:07 +0200

    step-9

--- Workshop/Assets/Scripts/EnemyStats.cs
+++ Workshop/Assets/Scripts/EnemyStats.cs
@@ -1,14 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyStats : MonoBehaviour
 {
+    private float maxHealth = 10;
     private float health = 10;
+
+    public RectTransform healthBar;
 
     public void Damage(float damage)
     {
         health -= damage;
+        healthBar.sizeDelta = new Vector2(health / maxHealth * 200, 10);
+
         if (health <= 0)
         {
             Destroy(gameObject);
