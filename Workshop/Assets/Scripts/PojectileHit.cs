49b03910518aa1af1fb0aacd2ab35c82f7b481a6
marcelloroehrssen <marcello.roehrssen@gmail.com>
2019-05-08 15:13:18 +0200

    step-8

---
+++ Workshop/Assets/Scripts/PojectileHit.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PojectileHit : MonoBehaviour
+{
+    private float damage = 1f;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Enemy")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
