db03ed7b09238ddcb90e8cd154b9acc843eca5a5
marcelloroehrssen <marcello.roehrssen@gmail.com>
2019-05-08 03:58:35 +0200

    step-5

---
+++ Workshop/Assets/Scripts/Shooter.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    public GameObject projectile;
+    [Range(1,50)]
+    public float projectileSpeed = 10f;
+    
+    void FixedUpdate()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 rot = transform.rotation.eulerAngles * Mathf.Deg2Rad;
+            Vector3 dir = new Vector3(Mathf.Cos(rot.z),  Mathf.Sin(rot.z));
+            dir.Normalize();
+
+            GameObject proj = Instantiate(projectile);
+            proj.transform.position = transform.position;
+            proj.GetComponent<Rigidbody2D>().AddForce(
+                dir * projectileSpeed,
+                ForceMode2D.Impulse
+            );
+        }
+    }
+}
